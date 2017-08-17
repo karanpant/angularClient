8343f94035dd6240db41c2e3bac9d99da2ae2bd7
kpant <karanpant@outlook.com>
2017-08-17 22:06:41 +0545

    added roll

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -14,6 +14,7 @@ import { Component } from '@angular/core';
 
 export class SandboxComponent {
     name = 'Karan Pant';
+    roll=20;
     age = 36;
     person = {firstName: 'Steve', lastName:'Smith'}
 
