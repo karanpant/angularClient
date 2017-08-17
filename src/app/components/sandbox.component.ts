9a06c4c6344a9d575f9b74061b982b95e579cf30
kpant <karanpant@outlook.com>
2017-08-17 22:02:20 +0545

    AngularApp Added

---
+++ src/app/components/sandbox.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sandbox',
+    template: `<h1>{{ name }} is {{ age }} years old.</h1>
+                <h2>My name is {{ person.firstName }} {{ person.lastName }}</h2>
+                <ul>
+                    <li>{{ 'Hello World' }}</li>
+                    <li>{{ 1 + 1 }}</li>
+                    <li>{{ showAge() }}
+                </ul>
+    `
+})
+
+export class SandboxComponent {
+    name = 'Karan Pant';
+    age = 36;
+    person = {firstName: 'Steve', lastName:'Smith'}
+
+    constructor() {
+        console.log('Constructor ran');
+        // this.age = 36;
+        this.hasBirthday();
+    }
+
+    hasBirthday(){
+        this.age += 1;
+
+    }
+
+    showAge():number{
+        return this.age;
+    }
+}
