5f3c2c0a4abd83b65016c5aecc401dc5dba93df0
kpant <karanpant@outlook.com>
2017-08-18 07:55:54 +0545

    templateUrl and styleUrl added

---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sandbox',
+    templateUrl: `./sandbox.component.html`,
+    styleUrls: [`./sandbox.component.css`]
+     /*
+    styles: [`
+        .special{
+            color:green;
+            font-size: 20px;
+            text-transform:uppercase
+        }
+    `]
+    */
+})
+
+export class SandboxComponent {
+  
+    
+}
+
+
